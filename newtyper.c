c2110b577a972b175a4476fb0ae5859d35e1e4db
Joshua Hudelson <joshua.hudelson@gmail.com>
2017-10-12 12:32:48 -0400

    Added some variables and note on zscore and ztable.

--- newtyper.c
+++ newtyper.c
@@ -1,18 +1,34 @@
 #include "m_pd.h"
 #include <math.h>
 
+#define NUM_STATES = 127;               // ASCII.
+#define MAX_NUM_OBSERVATIONS = 20;      // Maximum word length.
+#define MAX_NUM_TIMINGS = 100;          // Timings recorded before wrap-around.
+
 static t_class * newtyper_class;
 
 typedef struct _newtyper{
+
   t_object  x_obj;
-  t_float bigram_table[200][200];
+
+  t_float bigram_table[NUM_STATES][NUM_STATES];
   FILE * myfile;
-  t_int first, second;
   t_symbol * filepath;
+  t_int first, second;                  // For counting bigrams.
+
+  t_float timings_table[NUM_STATES][NUM_STATES][MAX_NUM_TIMINGS];
+  t_int timings_counter[NUM_STATES][NUM_STATES];  // total timings recorded so far.
+  t_int timings_placer[NUM_STATES][NUM_STATES]; // what position to add new timing.
+  t_float means[NUM_STATES][NUM_STATES];
+  t_float stdevs[NUM_STATES][NUM_STATES];
+
+  t_float ztable[4][10][10];                    //000 to 3.99
+
   }t_newtyper;
 
 
 void * newtyper_new(void) {
+
   char buff[50];
   t_newtyper *x = (t_newtyper *)pd_new(newtyper_class);
   sprintf(buff, "d%lx", (t_int)x);
@@ -20,17 +36,34 @@ void * newtyper_new(void) {
   pd_bind(&x->x_obj.ob_pd, x->filepath);
   outlet_new(&x->x_obj, &s_symbol);
   return (void *) x;
+
 }
+
+void do_viterbi(t_newtyper * x){
+
+  // the z-score is the number (float) of stdevs away from mean.
+  // the z-table translates z-scores into percentiles.
+  // the farther from the mean a timing measurement, the less weight it should have.
+  // but zscore is signed.  So make it always positive.
+  // then get the percentile from ztable.
+  // then subtract from 1.
+  // the result, if it's perfectly on the mean, will be 0.5
+  // So translate this, I guess, by multiplying by 2
+  // Now that's the likelihood that a given timing equals some letter, given the preceding one.
+
+}
+
 
 void newtyper_load(t_newtyper * x, t_symbol * s){
   char *path = (s && s->s_name) ? s->s_name : "\"\"";
   sys_vgui("pdtk_openpanel {%s} {%s}\n", x->filepath->s_name, path);
-
 }
+
 
 void newtyper_onbang(t_newtyper * x){
   newtyper_load(x, &s_);
 }
+
 
 static void newtyper_callback(t_newtyper *x, t_symbol *s)
 {
@@ -49,6 +82,8 @@ static void newtyper_callback(t_newtyper *x, t_symbol *s)
       x->first = x->second;
     }
 
+    post("How many s to t's: %f", x->bigram_table[115][116]);
+
     for(int i=0;i<200;i++){
       float row_sum = 0;
       for(int j=0;j<200;j++){
@@ -60,8 +95,9 @@ static void newtyper_callback(t_newtyper *x, t_symbol *s)
         }
       }
     }
+
     post("OK!");
-    post("97-98: %f", x->bigram_table[97][115]);
+    post("97-98: %f", x->bigram_table[115][116]);
 }
 
 void newtyper_setup(void) {
